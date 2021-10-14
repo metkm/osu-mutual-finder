524fd9f81f9b5e2f79b1ab6fe2442376fa15ec57
Sibyl666 <metinkorkmaz417@gmail.com>
2021-10-14 21:13:04 +0300

    remove unused directive

--- src/main.ts
+++ src/main.ts
@@ -5,30 +5,8 @@ import Store from "./store/index";
 import Notification from "./plugin/notification";
 import "./index.css";
 
-const app = createApp(App);
-
-app.directive("transition", {
-  mounted(el: HTMLElement) {
-    window.onresize = () => {
-      const elements = Array.from(el.children) as HTMLElement[];
-
-      let leftOffset = 20;
-      elements.forEach(element => {
-        element.style.left = `${leftOffset}px`;
-        leftOffset += (element.clientWidth + 10);
-  
-        element.onclick = () => {
-          element.setAttribute("style", "top: 0px; left: 0px; width: 100%; height: 100%; z-index: 1; border-radius: 1px;");
-          element.innerHTML = "";
-        }
-      })
-    };
-
-    window.dispatchEvent(new Event("resize"))
-  }
-})
-
-app.use(Router);
-app.use(Store);
-app.use(Notification);
-app.mount("#app");
+createApp(App)
+.use(Router)
+.use(Store)
+.use(Notification)
+.mount("#app");
