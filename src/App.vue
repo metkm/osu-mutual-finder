d4f293ce684e08e83d6202df6eba8e6aa9de9188
Sibyl <metinkorkmaz417@gmail.com>
2022-05-06 12:45:23 +0300

    push to settings if dev mode is enabled

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 
 if (import.meta.env.DEV) {
-  router.push({ path: "/" });
+  router.push({ path: "/settings" });
 } else {
   router.push({ path: "/" });
 }
