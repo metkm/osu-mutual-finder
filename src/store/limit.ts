07d827c296b135218c12ad9c67cbaac488879745
Sibyl <metinkorkmaz417@gmail.com>
2022-06-03 00:58:57 +0300

    fix

--- src/store/limit.ts
+++ src/store/limit.ts
@@ -6,21 +6,21 @@ import { StoreState } from "../types";
 import { clampNumber } from "../utils";
 
 interface Limit {
-  countryCode: string,
-  start: number, // page start
-  end: number, // page end
-  index: number // start index (only for start page)
+  countryCode: string;
+  start: number; // page start
+  end: number; // page end
+  index: number; // start index (only for start page)
 }
 
 export interface LimitState {
-  limits: Limit[],
+  limits: Limit[];
 }
 
 export const clampLimit = (limit: Limit) => {
   limit.start = clampNumber(limit.start, 1, 200);
   limit.end = clampNumber(limit.end, 1, 200);
   limit.index = clampNumber(limit.index, 0, 50);
-}
+};
 
 const limit: Module<LimitState, StoreState> = {
   state: () => ({
@@ -35,20 +35,26 @@ const limit: Module<LimitState, StoreState> = {
       let index = limits.findIndex(limit => limit.countryCode == code);
       limits.splice(index, 1);
     },
-    updateLimit({ limits }, newLimit: Limit) {
-      clampLimit(newLimit);
-
-      let index = limits.findIndex(limit => limit.countryCode == newLimit.countryCode);
+    updateLimit({ limits }, { data, index }: { data: Limit; index: number }) {
+      clampLimit(data);
+      limits.splice(index, 1, data);
+    },
+  },
+  actions: {
+    updateLimit({ commit, state }, newLimit: Limit) {
+      let index = state.limits.findIndex(limit => limit.countryCode == newLimit.countryCode);
       if (index !== -1) {
-        limits.splice(index, 1, newLimit);
+        commit("updateLimit", { data: newLimit, index });
+      } else {
+        commit("addLimit", newLimit);
       }
-    }
+    },
   },
   getters: {
-    getLimit: (state) => (code: string) => {
+    getLimit: state => (code: string) => {
       return state.limits.find(limit => limit.countryCode == code);
-    }
-  }
-}
+    },
+  },
+};
 
 export default limit;
