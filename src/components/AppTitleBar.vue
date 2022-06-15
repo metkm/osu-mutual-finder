1c4d0497b45e22d8f289baf33fb266d18ea74018
Sibyl <metinkorkmaz417@gmail.com>
2022-06-15 18:12:14 +0300

    wider title bar buttons

--- src/components/AppTitleBar.vue
+++ src/components/AppTitleBar.vue
@@ -10,19 +10,19 @@ onMounted(() => {
 <template>
   <div data-tauri-drag-region style="-webkit-app-region: drag" class="w-full h-5 shadow-sm dark:bg-neutral-800 flex-shrink-0 flex justify-end">
     <div class="h-full" @click="appWindow.minimize">
-      <svg class="hover:bg-neutral-200 dark:hover:bg-neutral-900 h-full w-6" viewBox="0 0 40 40" fill="none">
+      <svg class="hover:bg-neutral-200 dark:hover:bg-neutral-900 h-full w-7" viewBox="0 0 40 40" fill="none">
         <rect x="9" y="18" width="22" height="4" rx="2" class="fill-black dark:fill-white" /> 
       </svg>
     </div>
 
     <div class="h-full" @click="appWindow.toggleMaximize">
-      <svg class="hover:bg-neutral-200 dark:hover:bg-neutral-900 h-full w-6" fill="none" viewBox="0 0 40 40" >
+      <svg class="hover:bg-neutral-200 dark:hover:bg-neutral-900 h-full w-7" fill="none" viewBox="0 0 40 40" >
         <rect x="10" y="10" width="20" height="20" rx="2" class="dark:stroke-white stroke-black" stroke-width="3"/>
       </svg>
     </div>
 
     <div class="h-full" @click="appWindow.close">
-      <svg class="hover:bg-red-500 h-full w-6 " viewBox="0 0 40 40">
+      <svg class="hover:bg-red-500 h-full w-7 " viewBox="0 0 40 40">
         <path d="M10 10L30 30" class="dark:stroke-white stroke-black" stroke-width="3" stroke-linecap="round"/>
         <path d="M30 10L10 30" class="dark:stroke-white stroke-black" stroke-width="3" stroke-linecap="round"/>
       </svg>
