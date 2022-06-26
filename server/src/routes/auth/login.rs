d0a43939c961861323afc2259594ce4eea2a86fb
Sibyl <metinkorkmaz417@gmail.com>
2022-06-27 00:11:52 +0300

    fix login redirect_uri

--- server/src/routes/auth/login.rs
+++ server/src/routes/auth/login.rs
@@ -11,7 +11,7 @@ pub async fn login() -> Result<impl IntoResponse, impl IntoResponse> {
     if cfg!(debug_assertions) {
         params.push(("redirect_uri", "http://localhost:3001/api/authorize"));
     } else {
-        params.push(("redirect_uri", "https://tauri.localhost/api/authorize"));
+        params.push(("redirect_uri", "https://sibylku.xyz/api/authorize"));
     }
 
     let Ok(url) = reqwest::Url::parse_with_params("https://osu.ppy.sh/oauth/authorize", &params) else {
