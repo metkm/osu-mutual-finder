965e1bd11c5242911106d912a5b44ce0476b5784
Sibyl <metinkorkmaz417@gmail.com>
2022-11-22 17:36:48 +0300

    update versions

--- src/components/AppCheckbox.vue
+++ src/components/AppCheckbox.vue
@@ -5,12 +5,20 @@ defineProps<{
   toggled: boolean,
   onChange: () => void;
 }>();
+
+defineEmits(["update:modelValue"]);
 </script>
 
 <template>
   <div class="flex flex-col items-center">
     <div class="flex items-center gap-2">
-      <input type="checkbox" :id="label" v-model="toggled" class="form-tick" @change="onChange">
+      <input 
+        type="checkbox"
+        :id="label"
+        class="form-tick"
+        @change="onChange"
+        @input="$emit('update:modelValue', ($event.target! as HTMLInputElement).value)"
+      >
       <label :for="label">{{ label }}</label>
     </div>
     <p class="setting-description">{{ description }}</p>
