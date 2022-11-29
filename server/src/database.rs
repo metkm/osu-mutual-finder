e2f57b66594c4e9bb0caee208b8f20fea92566da
Sibyl <metinkorkmaz417@gmail.com>
2022-11-29 17:49:31 +0300

    load different .env file on release

--- server/src/database.rs
+++ server/src/database.rs
@@ -1,10 +1,16 @@
-use dotenvy::dotenv;
+use dotenvy;
 
 use diesel::PgConnection;
 use diesel::r2d2::{ConnectionManager, Pool};
 
 pub fn establish_connection_pool() -> Pool<ConnectionManager<PgConnection>> {
-    dotenv().expect("Couldn't load .env file");
+    let env_file = if cfg!(debug_assertions) {
+        ".dev.env"
+    } else {
+        ".prod.env"
+    };
+
+    dotenvy::from_filename(env_file).expect("Couldn't load .env file");
 
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL env varible must be set");
 
