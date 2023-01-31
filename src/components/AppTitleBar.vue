db6224f42d9e9f3e049bc03715598dde803d9c79
metkm <metinkorkmaz417@gmail.com>
2023-01-31 14:19:20 +0300

    animation to back button

--- src/components/AppTitleBar.vue
+++ src/components/AppTitleBar.vue
@@ -24,14 +24,17 @@ appWindow.onResized(async () => {
 </script>
 
 <template>
-  <nav data-tauri-drag-region id="titlebar" class="h-8 flex items-center justify-between select-none text-xs border-b dark:border-neutral-800">
-    <div data-tauri-drag-region class="flex-1 h-full">
-      <button aria-label="go back" class="window-control-btn" v-if="route.name == 'Settings'" @click="router.back">
-        <Back />
-      </button>
-    </div>
-
-    <!-- <p data-tauri-drag-region class="flex-1 truncate flex justify-center items-center drop-shadow">Mutual Finder - {{ currentRoute }}</p> -->
+  <nav data-tauri-drag-region id="titlebar" class="h-8 relative flex items-center justify-between select-none text-xs border-b dark:border-neutral-800">
+    <TransitionGroup data-tauri-drag-region name="buttons" tag="div" class="flex flex-1 h-full items-center justify-start">
+      <div v-if="route.name == 'Settings'" class="h-full">
+        <button aria-label="go back" class="window-control-btn" @click="router.back">
+          <Back />
+        </button>
+      </div>
+  
+      <p data-tauri-drag-region class="pl-2">Mutual Finder - {{ route.name }}</p>
+      <!-- <p data-tauri-drag-region class="flex-1 truncate flex justify-center items-center drop-shadow">Mutual Finder - {{ route.name }}</p> -->
+    </TransitionGroup>
 
     <div data-tauri-drag-region class="h-full flex-1 flex items-center justify-end">
       <button aria-label="minimize window" class="window-control-btn" @click="appWindow.minimize">
@@ -49,3 +52,21 @@ appWindow.onResized(async () => {
     </div>
   </nav>
 </template>
+
+<style scoped>
+.buttons-leave-active, 
+.buttons-enter-active,
+.buttons-move {
+  transition: all 350ms ease-in-out;
+}
+
+.buttons-leave-active {
+  position: absolute;
+}
+
+.buttons-enter-from, .buttons-leave-to {
+  transform: translateX(-100px);
+  opacity: 0;
+}
+
+</style>
