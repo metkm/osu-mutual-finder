cbbb81bd861a542ae29b7e72fead021c3fcaf376
metkm <metinkorkmaz417@gmail.com>
2023-02-05 22:47:09 +0300

    clean login page

--- src/components/Login/LoginMutuals.vue
+++ src/components/Login/LoginMutuals.vue
@@ -1,3 +1,33 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useAuthStore } from '../../store';
+import { UserObject } from "../../types";
+import axios from "axios";
+
+import AppList from "../AppList.vue";
+import User from "../User.vue";
+
+const authStore = useAuthStore();
+const users = ref<UserObject[]>([]);
+
+if (authStore.access_token) {
+  const response = await axios.get<UserObject[]>("/api/mutuals");
+  users.value = response.data;
+}
+</script>
+
 <template>
-  <p>mutuals</p>
+  <section 
+    v-if="users.length > 0" 
+    class="flex flex-col w-full max-h-96 overflow-hidden rounded border dark:border-neutral-800"
+  >
+    <div class="p-2">
+      <p>Found Mutuals</p>
+      <p class="text-sm text-neutral-500">mutuals that are found from the database of mutual finder.</p>
+    </div>
+
+    <AppList :items="users" :itemHeight="76" v-slot="{ item }">
+      <User :user="item" />
+    </AppList>
+  </section>
 </template>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,6 +8,7 @@ import BaseSuspense from "../components/Ui/BaseSuspense.vue";
 import BaseButton from "../components/Ui/BaseButton.vue";
 import BaseInput from "../components/Ui/BaseInput.vue";
 import IconLogin from "../components/Icons/Login.vue";
+import LoginMutuals from "../components/login/LoginMutuals.vue";
 
 const username = ref("");
 const password = ref("");
@@ -26,92 +27,11 @@ const loginHandler = async () => {
     router.push("/verify");
   }
 }
-
-
-// const username = ref("");
-// const password = ref("");
-// const cooldown = ref(false);
-// const version = await app.getVersion();
-// const settingsStore = useSettingsStore();
-// const authStore = useAuthStore();
-// const userStore = useUserStore();
-// const mutuals = ref<UserObject[] | null>();
-
-// if (authStore.access_token) {
-//   axios.get<UserObject[]>("/api/mutuals")
-//   .then(users => {
-//     mutuals.value = users.data;
-//   });
-// }
-
-// const login = async () => {
-//   if (!username.value && !password.value) return;
-
-//   cooldown.value = true;
-//   const client = await http.getClient();
-//   const response = await client.get("https://osu.ppy.sh/home", { responseType: 2 });
-
-//   let cookies = getCookies(response.rawHeaders);
-//   let cookieString = parseCookies(cookies);
-
-//   const sessionResponse = await client.post<Login>("https://osu.ppy.sh/session", {
-//     payload: {
-//       "_token": cookies["XSRF-TOKEN"],
-//       "username": username.value,
-//       "password": password.value
-//     },
-//     type: "Form"
-//   }, {
-//     headers: {
-//       "referer": "https://osu.ppy.sh",
-//       "cookie": cookieString
-//     }
-//   });
-
-//   cooldown.value = false;
-//   // // error handling here.
-//   if (sessionResponse.status !== 200) {
-//     notify(`Login request returned ${sessionResponse.status} code.`, {
-//       description: "This is probably because of a change made on osu! website. You can open a github issue."
-//     });
-
-//     return;
-//   }
-
-//   if (!userStore.user) {
-//     settingsStore.toggleBlacklistId(sessionResponse.data.user.id)
-//   }
-
-//   userStore.user = sessionResponse.data.user;
-//   cookies = getCookies(sessionResponse.rawHeaders);
-//   cookieString = parseCookies(cookies);
-
-//   const verifResponse = await client.get("https://osu.ppy.sh/home/account/edit", {
-//     headers: {
-//       "cookie": cookieString
-//     }
-//   })
-
-//   if (verifResponse.status !== 401) {
-//     notify(`Verification request returned ${verifResponse.status} code.`, {
-//       description: "The expected code is 401"
-//     });
-
-//     return;
-//   }
-
-//   cookies = getCookies(verifResponse.rawHeaders);
-//   authStore.session = cookies["osu_session"];
-//   authStore.token = cookies["XSRF-TOKEN"];
-
-//   router.push("/verify");
-// }
-
 </script>
 
 <template>
-  <div class="page flex justify-center items-center">
-    <form aria-label="login form" class="grid gap-2 w-full max-w-lg text-sm">
+  <div class="page flex flex-col justify-center items-center gap-4 max-w-lg mx-auto overflow-hidden">
+    <form aria-label="login form" class="grid gap-2 w-full text-sm">
       <div class="flex flex-col">
         <label for="username" class="ml-1">Username</label>
         <BaseInput id="username" v-model="username" required />
@@ -135,16 +55,9 @@ const loginHandler = async () => {
         </BaseButton>
       </div>
     </form>
-  </div>
 
-    <!-- <section
-      v-if="mutuals && mutuals.length > 0"
-      class="max-h-96 flex flex-col"
-    >
-      <h1 class="text-center mt-4">Found mutuals from the database</h1>
-      <ul class="flex flex-1 flex-col gap-1 overflow-y-auto">
-        <User v-for="user in mutuals" :user="user" :userId="user.id" />
-      </ul>
-    </section> -->
-  <!-- </div> -->
+    <BaseSuspense>
+      <LoginMutuals />
+    </BaseSuspense>
+  </div>
 </template>
