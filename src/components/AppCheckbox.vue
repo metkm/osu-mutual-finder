a954f54bf473078eed24a0e68278398050392003
metkm <metinkorkmaz417@gmail.com>
2023-02-09 15:25:02 +0300

    light theme for checkbox

--- src/components/AppCheckbox.vue
+++ src/components/AppCheckbox.vue
@@ -18,7 +18,7 @@ const updateModelValue = (event: Event) => {
 <template>
   <div class="flex flex-col items-center gap-2">
     <div class="flex items-center gap-2">
-      <div class="w-14 p-1 rounded-full relative bg-neutral-800 transition-colors" :class="{ '!bg-green-600': modelValue }">
+      <div class="w-12 p-1 rounded-full relative bg-neutral-200 dark:bg-neutral-800 transition-colors" :class="{ '!bg-green-600': modelValue }">
         <input
           type="checkbox"
           :id="label"
@@ -26,7 +26,10 @@ const updateModelValue = (event: Event) => {
           @change="updateModelValue"
           class="absolute inset-0 appearance-none"
         >
-        <div class="h-3 w-3 rounded-full bg-white transition-all pointer-events-none" :class="{ 'translate-x-9': modelValue }" />
+        <div 
+          class="h-3 w-3 rounded-full bg-neutral-400 dark:bg-white transition-all pointer-events-none" 
+          :class="{ 'translate-x-7 !bg-white': modelValue }"
+        />
       </div>
       <label :for="label">{{ label }}</label>
     </div>
