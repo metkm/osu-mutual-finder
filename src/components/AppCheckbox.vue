e276fdaa5cbe3e225a22682f01537953c880ab5f
metkm <metinkorkmaz417@gmail.com>
2023-02-09 13:33:23 +0300

    update checkbox with slider

--- src/components/AppCheckbox.vue
+++ src/components/AppCheckbox.vue
@@ -1,20 +1,36 @@
 <script setup lang="ts">
-defineProps(['modelValue', 'label', 'description'])
-defineEmits(["update:modelValue"]);
+defineProps<{
+  modelValue: boolean,
+  label: string,
+  description: string
+}>();
+const emit = defineEmits<{
+  (e: "update:modelValue", value: boolean): void
+}>();
+
+const updateModelValue = (event: Event) => {
+  if (!event.target) return;
+  let target = event.target as HTMLInputElement;
+  emit("update:modelValue", target.checked);
+}
 </script>
 
 <template>
-  <div class="flex flex-col items-center">
+  <div class="flex flex-col items-center gap-2">
     <div class="flex items-center gap-2">
-      <input 
-        type="checkbox"
-        class="form-tick"
-        :id="label"
-        :checked="modelValue"
-        @change="$emit('update:modelValue', ($event.target! as HTMLInputElement).checked)"
-      >
+      <div class="w-14 p-1 rounded-full relative bg-neutral-800 transition-colors" :class="{ '!bg-green-600': modelValue }">
+        <input
+          type="checkbox"
+          :id="label"
+          :checked="modelValue"
+          @change="updateModelValue"
+          class="absolute inset-0 appearance-none"
+        >
+        <div class="h-3 w-3 rounded-full bg-white transition-all pointer-events-none" :class="{ 'translate-x-9': modelValue }" />
+      </div>
       <label :for="label">{{ label }}</label>
     </div>
+
     <p class="text-neutral-500">{{ description }}</p>
   </div>
 </template>
