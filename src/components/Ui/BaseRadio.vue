d84876daf88e999fb229f6ca7c4e339b3d14c587
metkm <metinkorkmaz417@gmail.com>
2023-02-10 16:42:42 +0300

    add hover effect to baseradio

--- src/components/Ui/BaseRadio.vue
+++ src/components/Ui/BaseRadio.vue
@@ -6,15 +6,10 @@ defineProps<{
 </script>
 
 <template>
-  <div class="px-4 py-2 relative rounded transition-colors" :class="{ 'bg-green-600 text-white': modelValue === label }">
-    <input 
-      class="appearance-none absolute inset-0" 
-      type="radio"
-      :id="label"
-      :value="label"
-      :checked="modelValue == label"
-      @change="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
-    />
+  <div class="px-4 py-2 relative rounded hover:bg-neutral-100 hover:dark:bg-neutral-800 transition-colors"
+    :class="{ 'bg-green-600 text-white': modelValue === label }">
+    <input class="appearance-none absolute inset-0" type="radio" :id="label" :value="label"
+      :checked="modelValue == label" @change="$emit('update:modelValue', ($event.target as HTMLInputElement).value)" />
     <label :for="label">{{ label }}</label>
   </div>
 </template>
