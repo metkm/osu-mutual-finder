e10febeba49a16ce1d473889412b8b3723f67c85
metkm <metinkorkmaz417@gmail.com>
2023-02-11 13:04:44 +0300

    update titlebar design little

--- src/components/AppTitleBar.vue
+++ src/components/AppTitleBar.vue
@@ -24,22 +24,16 @@ appWindow.onResized(async () => {
 </script>
 
 <template>
-  <nav 
-    data-tauri-drag-region 
-    id="titlebar" 
-    class="relative flex items-center h-8 text-xs select-none border-b dark:border-neutral-800"
-    >
-    <TransitionGroup data-tauri-drag-region name="buttons" tag="div" class="flex flex-1 h-full items-center justify-start">
-      <div v-if="route.name == 'Settings'" key="control" class="h-full">
-        <button aria-label="go back" class="window-control-btn" @click="router.back">
-          <Back />
-        </button>
-      </div>
-  
-      <p data-tauri-drag-region key="title" class="pl-2">Mutual Finder - {{ route.name }}</p>
-    </TransitionGroup>
+  <nav data-tauri-drag-region id="titlebar" 
+    class="h-8 grid grid-flow-col auto-cols-fr items-center border-b dark:border-neutral-800 select-none text-xs"
+  >
+    <button aria-label="go back" class="window-control-btn" :class="{ 'opacity-30 pointer-events-none': route.name != 'Settings' }" @click="router.back">
+      <Back />
+    </button>
 
-    <div data-tauri-drag-region class="h-full flex-1 flex items-center justify-end">
+    <p data-tauri-drag-region class="pl-2 text-center text-neutral-500">Mutual Finder - {{ route.name }}</p>
+
+    <div data-tauri-drag-region class="h-full flex justify-end">
       <button aria-label="minimize window" class="window-control-btn" @click="appWindow.minimize">
         <Minimize />
       </button>
@@ -55,21 +49,3 @@ appWindow.onResized(async () => {
     </div>
   </nav>
 </template>
-
-<style scoped>
-.buttons-leave-active, 
-.buttons-enter-active,
-.buttons-move {
-  transition: all 350ms ease-in-out;
-}
-
-.buttons-leave-active {
-  position: absolute;
-}
-
-.buttons-enter-from, .buttons-leave-to {
-  transform: translateX(-100px);
-  opacity: 0;
-}
-
-</style>
