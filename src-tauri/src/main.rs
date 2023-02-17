0dab56cce163a95fe131478ca4f9846f55325d8e
metkm <metinkorkmaz417@gmail.com>
2023-02-17 16:16:51 +0300

    sleep thread on window resize for webview2 performance

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,17 +3,28 @@
     windows_subsystem = "windows"
 )]
 
-use tauri::Manager;
+use tauri::{Manager, WindowEvent};
 use window_shadows::set_shadow;
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            if let Some(window) = app.get_window("main") {
-                if set_shadow(&window, true).is_err() {
-                    println!("error while adding shadow to window");
-                }
+            let Some(window) = app.get_window("main") else {
+                return Ok(())
+            };
+
+            if set_shadow(&window, true).is_err() {
+                println!("error while adding shadow to main window");
             }
+
+            window.on_window_event(|event| {
+                match event {
+                    WindowEvent::Resized(..) => {
+                        std::thread::sleep(std::time::Duration::from_micros(500)) // 0.5ms
+                    }
+                    _ => {}
+                }
+            });
 
             Ok(())
         })
