7a2bfb461ba9f8b21312015280acd48ac71f41de
metkm <metinkorkmaz417@gmail.com>
2023-02-17 19:48:20 +0300

    change text to black on hover when mode is selected

--- src/components/Ui/BaseRadio.vue
+++ src/components/Ui/BaseRadio.vue
@@ -7,7 +7,7 @@ defineProps<{
 
 <template>
   <div class="px-4 py-2 relative rounded hover:bg-neutral-100 hover:dark:bg-neutral-800 transition-colors"
-    :class="{ 'bg-green-600 text-white': modelValue === label }">
+    :class="{ 'bg-green-600 text-white hover:text-black': modelValue === label }">
     <input class="appearance-none absolute inset-0" type="radio" :id="label" :value="label"
       :checked="modelValue == label" @change="$emit('update:modelValue', ($event.target as HTMLInputElement).value)" />
     <label :for="label">{{ label }}</label>
